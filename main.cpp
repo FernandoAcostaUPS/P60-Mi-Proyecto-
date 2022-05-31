50705238550a99d38d1aef1e2ffc6f7c8845a3a8
Fernando Acosta <jacostar2@est.ups.edu.ec>
2022-05-31 10:34:23 -0500

    Creada la clase persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "persona.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en GIT y GITHUB!!" << endl;
+    Persona p;
+    p.nombre =  "Fernando";
+    p.edad = 23;
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
